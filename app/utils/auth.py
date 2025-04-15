9bb126e2c6a10bf1ee283176d4c0d591ccc7afdc
nody <dinokrcicprof@gmail.com>
2025-04-15 16:27:26 +0200

    auth dependencie finished

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -1,20 +1,30 @@
 from typing import Annotated
 from fastapi.security import OAuth2PasswordBearer
 import jwt
+from fastapi import HTTPException
 from routers import auth
 import logging
 from fastapi import Depends
+from database import get_db
+from sqlalchemy.orm import Session
+from models import User
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
 
 
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+async def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)], db: Session = Depends(get_db)
+):
     logging.warning(token)
     payload = jwt.decode(jwt=token, key=auth.SECRET_KEY, algorithms=[auth.ALGORITHM])
     logging.warning(payload)
-    #     {'userData': {'id': 15, 'email': 'shiredude@gmail.com', 'created_at': 1744565888.052378, 'username': 'bilboBagins311'}, 'expires_at': 1745310301.156434}
-    # WARNING:root:{'user': 'test'}
-    # check if user exists in db
-    # if so return the user
-    # if not throw error
-    return {"user": "test"}
+
+    if not payload["userData"]:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    user = db.query(User).filter(User.id == payload["userData"]["id"]).first()
+
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    return {"user": user}
