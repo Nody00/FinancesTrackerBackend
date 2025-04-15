2f64ddde07bc813a823b4575c0effc600c9a5522
nody <dinokrcicprof@gmail.com>
2025-04-15 16:58:29 +0200

    deleted placeholder route

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
-from database import get_db, engine
+from fastapi import FastAPI
+from database import engine
 import models
 from routers import router as api_router
 
@@ -9,9 +8,3 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(api_router, prefix="/api")
-
-
-@app.get("/users", status_code=200)
-async def get_users(db: Session = Depends(get_db)):
-    items = db.query(models.User).all()
-    return {"users": items}
